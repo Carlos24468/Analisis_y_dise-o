a7931aeefb7cfbd4ffdf9ebc16ee23e7f6324639
Usuario <Usuario@DESKTOP-4BIK5G7>
2025-08-29 23:08:28 -0500

    alma

--- src/tienda2/Almacen.java
+++ src/tienda2/Almacen.java
@@ -2,7 +2,25 @@
 package tienda2;
 
 public class Almacen {
-    //agregar productos 
+    Productos [] producto;
+    int cont;
+    public Almacen(int cap){
+        producto=new Productos[cap]; 
+        cont=0;
+    }
+    // agregar un producto
+    public void agrepro(Productos p){
+        if(cont<producto.length){
+            producto[cont]=p;
+            cont++;
+            System.out.println("Procucto agregado");
+        }
+        else{
+            System.out.println("Capacidad maxima alcansada");
+        }
+        
+    }
+    //agregar productos :echo
     //poner una lista de los productos si no hay productos le dira que no hay productos registrados
     //usar el mettodo burbuja para ordenar nombres 
     //ordenar por precios
